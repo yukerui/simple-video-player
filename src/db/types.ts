9997bbb45ab6ac9ef40cb7f9a8b279cbf2195edc
ltxlong <416803647@qq.com>
2025-04-03 02:14:47 +0800

    Delete src/db/types.ts

--- src/db/types.ts
+++
@@ -1,9 +0,0 @@
-import { JsonAdapterOptions } from './adapters/json-adapter.js';
-import { PgAdapterOptions } from './adapters/pg-adapter.js';
-
-export interface DatabaseConfig {
-  sqlDsn?: string;
-  pgConfig?: PgAdapterOptions;
-  kvNamespace?: any;
-  jsonConfig?: JsonAdapterOptions;
-} 
